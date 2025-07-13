3f565898db8d6667cc810b41683cbac265595a12
taroAsahina <taro.asahina@uzabase.com>
2025-07-13 13:17:06 +0900

    partical function

--- nattoku/src/nattoku/currying.clj
+++ nattoku/src/nattoku/currying.clj
@@ -14,14 +14,21 @@
       data
       (throw (ex-info "Validation failed" {:field field-name})))))
 
+(defn validate-field2 [field-name validation-fn]
+  #(if (validation-fn (get % field-name))
+     %
+     (throw (ex-info "Validation failed" {:field field-name}))))
+
 (def validate-email (validate-field :email #(re-matches #"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$" %)))
-(def validate-age (validate-field :age #(> % 0)))
+(def validate-age (validate-field2 :age #(> % 0)))
 
 (comment
   (add 1 2)
   ((curried-add 1) 2)
   (((curried-add 1) 2) 3)
+  ((((curried-add 1) 2) 3) 4) 
 
   (validate-email {:email "test@example.com"})
   (validate-age {:age 20})
   (validate-age {:age -1})
+  )
